25231078d733cbdcf752ee0a6297ff150f444c7e
Rakesh Kumar <137685336+Rkiitb@users.noreply.github.com>
2026-01-03 10:31:58 +0530

    graph.py

--- graph.py
+++ graph.py
@@ -401,25 +401,3 @@ workflow = graph.compile(checkpointer=checkpointer)
 
 
 
-# CONFIG = {'configurable': {'thread_id': 'thread-1'}}
-# # session state for message history
-# if 'message_history' not in st.session_state:
-#     st.session_state['message_history'] = []
-
-
-# for message in st.session_state['message_history']:
-#     with st.chat_message(message['role']):
-#         st.text(message['content'])
-
-# query = st.chat_input("Ask about a stock or market:")
-
-# if query:
-#     st.session_state['message_history'].append({"role": "user", "content": query})
-#     with st.chat_message("user"):
-#         st.text(query)
-
-
-#     response=workflow.invoke({'user_query':query},config=CONFIG)
-#     st.session_state['message_history'].append({"role": "assistant", "content": response['final_result']})
-#     with st.chat_message("assistant"):
-#         st.markdown(response['final_result'])
